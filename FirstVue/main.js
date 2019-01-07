71f7b61a9bde6421346aff05c595f7ffd47025cf
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-01-07 16:11:27 -0500

    First Vue Example

---
+++ FirstVue/main.js
@@ -0,0 +1,17 @@
+const helloWorld = new Vue({
+	el: '#helloVue',
+	data: {
+		title: "Hello, world!",
+		message: "This is my first view Template!"
+	}
+});
+
+const example = new Vue({
+	el: '#example',
+	data: {
+		title: "Hello",
+		message: "I'm enjoying Vue ",
+		name: "Zak"
+	}
+});
+
